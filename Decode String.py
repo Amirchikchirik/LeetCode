892a583808d70c6ae92ad3d9aded27c56688408e
Amir Amanzholov <amiramanzholov95@gmail.com>
2025-02-06 22:37:18 +0500

    Update Decode String.py

--- Decode String.py
+++ Decode String.py
@@ -14,3 +14,14 @@ class Solution:
                     num = stack.pop() + num
                 stack.append(curr * int(num))
         return "".join(stack)
+# Example 1:
+# Input: s = "3[a]2[bc]"
+# Output: "aaabcbc"
+        
+# Example 2:
+# Input: s = "3[a2[c]]"
+# Output: "accaccacc"
+        
+# Example 3:
+# Input: s = "2[abc]3[cd]ef"
+# Output: "abcabccdcdcdef"
