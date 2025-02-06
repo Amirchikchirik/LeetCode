7fe1f30fdfc2746663db0bec6615b0531ec7e5d0
Amir Amanzholov <amiramanzholov95@gmail.com>
2025-02-06 22:02:46 +0500

    Update Contains Duplicate.py

--- Contains Duplicate.py
+++ Contains Duplicate.py
@@ -6,3 +6,8 @@ class Solution:
                 return True
             seen.add(num)
         return False
+# Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
+# Input: nums = [1,2,3,1]
+# Output: true
+# Input: nums = [1,2,3,4]
+# Output: false
