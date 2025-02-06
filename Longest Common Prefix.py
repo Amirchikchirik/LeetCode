22c8f1ba6a6f055d8eff887b0482677f61a935b0
Amir Amanzholov <amiramanzholov95@gmail.com>
2025-02-06 22:32:40 +0500

    Update Longest Common Prefix.py

--- Longest Common Prefix.py
+++ Longest Common Prefix.py
@@ -9,3 +9,8 @@ class Solution:
                 if i == len(word) or word[i] != baza[i]:
                     return baza[0:i]
         return baza
+# Write a function to find the longest common prefix string amongst an array of strings.
+# If there is no common prefix, return an empty string "".
+# examle:
+# Input: strs = ["flower","flow","flight"]
+# Output: "fl"
